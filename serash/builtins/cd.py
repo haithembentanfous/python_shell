e515430dce4bfd1e04d6fc29c94f9daf5cd32249
haithembentanfous <htanfous@gmail.com>
2017-01-21 16:38:33 +0100

    Update cd.py
    - handle error 
    - add "cd" with no args go to $HOME

--- serash/builtins/cd.py
+++ serash/builtins/cd.py
@@ -2,6 +2,15 @@ import os
 from serash.constants import *
 
 def cd(args):
-    os.chdir(args[0])
+    HOME = os.getenv("HOME")
+    
+    if not args :
+        os.chdir(HOME)
+        print (HOME)
+    else :
+        try :
+            os.chdir(args[0])
+        except OSError :
+            print ("Error path ! ")
 
     return stat_r
